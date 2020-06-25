f06445dfbc832e651f68faae96de8af8a1715852
Rey David Cuevas <cuevas.vela@gmail.com>
2020-06-25 11:41:29 -0500

    finished reverseString function

--- src/reverseString.js
+++ src/reverseString.js
@@ -1,3 +1,10 @@
 export default function reverseString(str) {
-    return 'gnirtsym';
+    if (typeof str != 'string'){
+        return str;
+    }
+    let str1 = "";
+    for (let i = str.length -1; i >= 0; i--){
+        str1 += str[i];
+    }
+    return str1;
 }
