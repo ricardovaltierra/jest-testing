7edef6580b139d4abd8ff5c2981d6550b341830c
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-06-25 15:41:02 -0500

    test suite for arrayAnalysis filled

--- src/arrayAnalysis.js
+++ src/arrayAnalysis.js
@@ -1,9 +1,24 @@
-export default function arrayAnalysis(arr){
-    const object = {
-        average: null,
-        min: null,
-        max: null,
-        length: null
-    };
-    return object;
+export default function arrayAnalysis(arr) {
+
+  if (arr === undefined || arr === null) {
+    throw new Error('Please insert an array of numbers.Please give both values');
+  } else if (typeof arr != 'array') {
+    throw new Error('Please insert an array of numbers.');
+  } 
+  // else if (typeof arr 'array') {
+  //   throw new Error('Please insert an array of numbers.');
+  // }
+
+  const object = {
+    average: null,
+    min: null,
+    max: null,
+    length: null
+  };
+
+  object.average = () => {
+    return (arr.reduce((acc, curr) => { acc + curr })) / arr.length;
+  }
+
+  return object;
 }
--- src/arrayAnalysis.test.js
+++ src/arrayAnalysis.test.js
@@ -1,7 +1,46 @@
 import arrayAnalysis from './arrayAnalysis';
 
-test('return and object', () => {
-    const array = [1,2,3,4,5,6]
-    expect(arrayAnalysis(array)).toBeTruthy();
+test('returns an object', () => {
+    const array = [1, 2, 3, 4, 5, 6];
+    const object = {
+        average: null,
+        min: null,
+        max: null,
+        length: null
+    };
+
+    expect(object).toEqual(arrayAnalysis(array));
 });
 
+test('throws error when nothing is provided', () => {
+    expect(() => arrayAnalysis()).toThrow('Please insert an array of numbers.');
+});
+
+test('throws error when no array is provided', () => {
+    expect(() => arrayAnalysis(10)).toThrow('Please insert an array of numbers.');
+});
+
+test('throws error when no array with just numbers is provided', () => {
+    const array = [1, 2, 3, 4, 5, a];
+    expect(() => arrayAnalysis(array)).toThrow('Please insert an array of just numbers.');
+});
+
+test('returns average of array', () => {
+    const array = [1, 2, 3, 4, 5, 6];
+    expect(arrayAnalysis(array).average).toEqual(3);
+});
+
+test('returns min value of array', () => {
+    const array = [1, 2, 3, 4, 5, 6];
+    expect(arrayAnalysis(array).min).toEqual(1);
+});
+
+test('returns max value of array', () => {
+    const array = [1, 2, 3, 4, 5, 6];
+    expect(arrayAnalysis(array).max).toEqual(6);
+});
+
+test('returns array\'s length', () => {
+    const array = [1, 2, 3, 4, 5, 6];
+    expect(arrayAnalysis(array).length).toEqual(6);
+});
