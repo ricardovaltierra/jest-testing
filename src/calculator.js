b91d3e5355ef416a5d4685b7810d1633c16f2946
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-06-25 11:25:59 -0500

    eslint & added calculator functions

---
+++ src/calculator.js
@@ -0,0 +1,15 @@
+export function add(num1, num2) {
+  return num1 + num2;
+}
+
+export function substract(num1, num2) {
+  return num1 - num2;
+}
+
+export function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+export function divide(num1, num2) {
+  return num1 / num2;
+}
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,34 +1,35 @@
-import { 
-  addition,
+import {
+  add,
   substract,
   multiply,
-  divide
+  divide,
 } from './calculator';
 
-test('addition (2 + 2 = 4)', () => {
-  expect(addition(2,2)).toEqual(4);
+
+test('add (2 + 2 = 4)', () => {
+  expect(add(2, 2)).toEqual(4);
 });
 
-test('addition range not equal zero', () => {
+test('add range not equal zero', () => {
   for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++)
-      expect(addition(i + j).not.toEqual(0));
-   }
+    for (let j = 0; j < 10; j++) expect(add(i + j).not.toEqual(0));
+  }
 });
 
-test('addition receives one value throws error', () => {
-  excpect(addition(2)).toThrow('Please give both values');
+test('add receives one value throws error', () => {
+  excpect(add(2)).toThrow('Please give both values');
 });
 
-test('addition strings throws error', () => {
-  excpect(addition('mystring','mystring')).toThrow('Please add just numbers');
+test('add strings throws error', () => {
+  excpect(add('mystring', 'mystring')).toThrow('Please add just numbers');
 });
+
 
 test('substract 2 - 2 = 0', () => {
   expect(substract(2, 2).toEqual(0));
 });
 
-test('nevative result', () => {
+test('substract nevative result', () => {
   expect(substract(2, -10).toEqual(-8));
 });
 
@@ -37,23 +38,24 @@ test('substract receives one value throws error', () => {
 });
 
 test('substract strings throws error', () => {
-  excpect(substract('mystring','mystring')).toThrow('Please add just numbers');
+  excpect(substract('mystring', 'mystring')).toThrow('Please add just numbers');
 });
+
 
 test('multiply 2 * 2 = 4', () => {
   expect(multiply(2, 2).toEqual(4));
 });
 
 test('multiply + * - = -', () => {
-  expect(multiply(2,-2).toBeLessThan(0));
+  expect(multiply(2, -2).toBeLessThan(0));
 });
 
 test('multiply - * - = +', () => {
-  expect(multiply(-2,-2).toBeGreaterThan(0));
+  expect(multiply(-2, -2).toBeGreaterThan(0));
 });
 
 test('multiply num * 0 = 0', () => {
-  expect(multiply(5,0).toEqual(0));
+  expect(multiply(5, 0).toEqual(0));
 });
 
 test('multiply receives one value throws error', () => {
@@ -61,11 +63,12 @@ test('multiply receives one value throws error', () => {
 });
 
 test('multiply strings throws error', () => {
-  excpect(multiply('mystring','mystring')).toThrow('Please add just numbers');
+  excpect(multiply('mystring', 'mystring')).toThrow('Please add just numbers');
 });
 
+
 test('divide 2 / 2 = 1', () => {
-  expect(didive(2, 2).toEqual(1));
+  expect(divide(2, 2).toEqual(1));
 });
 
 test('divide + * - = -', () => {
@@ -85,5 +88,5 @@ test('divide receives one value throws error', () => {
 });
 
 test('divide strings throws error', () => {
-  excpect(divide('mystring','mystring')).toThrow('Please add just numbers');
+  excpect(divide('mystring', 'mystring')).toThrow('Please add just numbers');
 });
