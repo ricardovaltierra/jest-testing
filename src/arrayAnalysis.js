249aff98fcbd42af18b99e1ca728698e3cbf51f3
Rey David Cuevas <cuevas.vela@gmail.com>
2020-06-25 14:54:40 -0500

    starting the array analisys test

---
+++ src/arrayAnalysis.js
@@ -0,0 +1,9 @@
+export default function arrayAnalysis(arr){
+    const object = {
+        average: null,
+        min: null,
+        max: null,
+        length: null
+    };
+    return object;
+}
---
+++ src/arrayAnalysis.test.js
@@ -0,0 +1,7 @@
+import arrayAnalysis from './arrayAnalysis';
+
+test('return and object', () => {
+    const array = [1,2,3,4,5,6]
+    expect(arrayAnalysis(array)).toBeTruthy();
+});
+
