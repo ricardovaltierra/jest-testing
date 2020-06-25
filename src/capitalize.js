ef0db0c0527ba96bdf753e692956cc2a06b2eb21
redacuve <53204984+redacuve@users.noreply.github.com>
2020-06-25 10:19:07 -0500

    add capitalize function

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,3 +1,18 @@
 export function capitalize(str){
-    return "Mystring";
+    if (typeof str != 'string'){
+        return str;
+    }
+    let str2 = "";
+    let firstLetter = true;
+    let i = 0;
+    str = str.toLowerCase();
+    while (i < str.length) {
+        if (str[i].match(/[a-z]/i) && firstLetter) {
+            str2 += str[i].toUpperCase();
+            firstLetter = false;
+        } else {
+            str2 += str[i]
+        }
+    }
+    return str2;
 }
