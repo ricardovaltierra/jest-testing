0d855e24d3ac0cb2dc7c5067befd7877f27d63a1
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-06-26 12:34:14 -0500

    refactored calculator function into a factory function / refactoring of test cases for calculator

--- src/calculator.js
+++ src/calculator.js
@@ -1,31 +1,49 @@
-export default function calculator(operator, num1, num2) {
-  if (num1 === undefined || num2 === undefined) {
-    throw new Error('Please give both values');
-  } else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    throw new Error('Please add just numbers');
-  } else {
-    let result = null;
-
-    switch (operator) {
-      case 'add':
-        result = num1 + num2;
-        break;
-      case 'substract':
-        result = num1 - num2;
-        break;
-      case 'multiply':
-        result = num1 * num2;
-        break;
-      case 'divide':
-        if (num2 === 0) {
-          throw new Error('Zero divition not possible');
-        }
-        result = num1 / num2;
-        break;
-      default:
-        break;
+const Calculator = (() => {
+  const add = (num1, num2) => {
+    if (num1 === undefined || num2 === undefined) {
+      throw new Error('Please give both values');
+    } else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+      throw new Error('Please add just numbers');
+    } else {
+      return num1 + num2;
     }
+  };
+  const substract = (num1, num2) => {
+    if (num1 === undefined || num2 === undefined) {
+      throw new Error('Please give both values');
+    } else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+      throw new Error('Please add just numbers');
+    } else {
+      return num1 - num2;
+    }
+  };
+  const multiply = (num1, num2) => {
+    if (num1 === undefined || num2 === undefined) {
+      throw new Error('Please give both values');
+    } else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+      throw new Error('Please add just numbers');
+    } else {
+      return num1 * num2;
+    }
+  };
+  const divide = (num1, num2) => {
+    if (num1 === undefined || num2 === undefined) {
+      throw new Error('Please give both values');
+    } else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+      throw new Error('Please add just numbers');
+    } else if (num2 === 0) {
+      throw new Error('Zero divition not possible');
+    } else {
+      return num1 / num2;
+    }
+  };
 
-    return result;
-  }
-}
+  return {
+    add,
+    substract,
+    multiply,
+    divide,
+  };
+})();
+
+export default Calculator;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,89 +1,88 @@
-import calculator from './calculator';
-
+import Calculator from './calculator';
 
 test('add (2 + 2 = 4)', () => {
-  expect(calculator('add', 2, 2)).toEqual(4);
+  expect(Calculator.add(2, 2)).toEqual(4);
 });
 
 test('add range not equal zero', () => {
   for (let i = 1; i <= 10; i += 1) {
     for (let j = 1; j <= 10; j += 1) {
-      expect(calculator('add', i, j)).not.toEqual(0);
+      expect(Calculator.add(i, j)).not.toEqual(0);
     }
   }
 });
 
 test('add receives one value throws error', () => {
-  expect(() => calculator('add', 1)).toThrow('Please give both values');
+  expect(() => Calculator.add(1)).toThrow('Please give both values');
 });
 
 test('add strings throws error', () => {
-  expect(() => calculator('add', 'mystring', 'mystring')).toThrow('Please add just numbers');
+  expect(() => Calculator.add('mystring', 'mystring')).toThrow('Please add just numbers');
 });
 
 
 test('substract 2 - 2 = 0', () => {
-  expect(calculator('substract', 2, 2)).toEqual(0);
+  expect(Calculator.substract(2, 2)).toEqual(0);
 });
 
 test('substract adds when first value negative', () => {
-  expect(calculator('substract', -10, 2)).toEqual(-12);
+  expect(Calculator.substract(-10, 2)).toEqual(-12);
 });
 
 test('substract receives one value throws error', () => {
-  expect(() => calculator('substract', 2)).toThrow('Please give both values');
+  expect(() => Calculator.substract(2)).toThrow('Please give both values');
 });
 
 test('substract strings throws error', () => {
-  expect(() => calculator('substract', 'mystring', 'mystring')).toThrow('Please add just numbers');
+  expect(() => Calculator.substract('mystring', 'mystring')).toThrow('Please add just numbers');
 });
 
 
 test('multiply 2 * 2 = 4', () => {
-  expect(calculator('multiply', 2, 2)).toEqual(4);
+  expect(Calculator.multiply(2, 2)).toEqual(4);
 });
 
 test('multiply + * - = -', () => {
-  expect(calculator('multiply', 2, -2)).toBeLessThan(0);
+  expect(Calculator.multiply(2, -2)).toBeLessThan(0);
 });
 
 test('multiply - * - = +', () => {
-  expect(calculator('multiply', -2, -2)).toBeGreaterThan(0);
+  expect(Calculator.multiply(-2, -2)).toBeGreaterThan(0);
 });
 
 test('multiply num * 0 = 0', () => {
-  expect(calculator('multiply', 5, 0)).toEqual(0);
+  expect(Calculator.multiply(5, 0)).toEqual(0);
 });
 
 test('multiply receives one value throws error', () => {
-  expect(() => calculator('multiply', 2)).toThrow('Please give both values');
+  expect(() => Calculator.multiply(2)).toThrow('Please give both values');
 });
 
 test('multiply strings throws error', () => {
-  expect(() => calculator('multiply', 'mystring', 'mystring')).toThrow('Please add just numbers');
+  expect(() => Calculator.multiply('mystring', 'mystring')).toThrow('Please add just numbers');
 });
 
 
 test('divide 2 / 2 = 1', () => {
-  expect(calculator('divide', 2, 2)).toEqual(1);
+  expect(Calculator.divide(2, 2)).toEqual(1);
 });
 
 test('divide + * - = -', () => {
-  expect(calculator('divide', 2, -2)).toBeLessThan(0);
+  expect(Calculator.divide(2, -2)).toBeLessThan(0);
 });
 
 test('divide - / - = +', () => {
-  expect(calculator('divide', -2, -2)).toBeGreaterThan(0);
+  expect(Calculator.divide(-2, -2)).toBeGreaterThan(0);
 });
 
 test('divide num / 0 = undefined', () => {
-  expect(() => calculator('divide', 5, 0)).toThrow('Zero divition not possible');
+  expect(() => Calculator.divide(5, 0)).toThrow('Zero divition not possible');
 });
 
 test('divide receives one value throws error', () => {
-  expect(() => calculator('divide', 2)).toThrow('Please give both values');
+  expect(() => Calculator.divide(2)).toThrow('Please give both values');
 });
 
 test('divide strings throws error', () => {
-  expect(() => calculator('divide', 'mystring', 'mystring')).toThrow('Please add just numbers');
+  expect(() => Calculator.divide('mystring', 'mystring')).toThrow('Please add just numbers');
 });
