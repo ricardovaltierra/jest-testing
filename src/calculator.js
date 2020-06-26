8ae28619c797126380414ebb8bed01d67056d8d0
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-06-26 09:55:23 -0500

    refactored functions into one object that receives operator

--- src/calculator.js
+++ src/calculator.js
@@ -1,37 +1,28 @@
-export function add(num1, num2) {
+export function calculator(operator, num1, num2) {
   if (num1 === undefined || num2 === undefined) {
     throw new Error('Please give both values');
   } else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
     throw new Error('Please add just numbers');
-  }
-  return num1 + num2;
-}
+  } else {
 
-export function substract(num1, num2) {
-  if (num1 === undefined || num2 === undefined) {
-    throw new Error('Please give both values');
-  } else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    throw new Error('Please add just numbers');
+    switch(operator) {
+      case 'add':
+        return num1 + num2;
+        break;
+      case 'substract':
+        return num1 - num2;
+        break;
+      case 'multiply':
+        return num1 * num2;
+        break;
+      case 'divide':
+        if (num2 === 0) {
+          throw new Error('Zero divition not possible');
+        }
+        return num1 / num2;
+        break;
+      default:
+        break;
+    }
   }
-  return num1 - num2;
-}
-
-export function multiply(num1, num2) {
-  if (num1 === undefined || num2 === undefined) {
-    throw new Error('Please give both values');
-  } else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    throw new Error('Please add just numbers');
-  }
-  return num1 * num2;
-}
-
-export function divide(num1, num2) {
-  if (num1 === undefined || num2 === undefined) {
-    throw new Error('Please give both values');
-  } else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    throw new Error('Please add just numbers');
-  } else if (num2 === 0) {
-    throw new Error('Zero divition not possible');
-  }
-  return num1 / num2;
 }
